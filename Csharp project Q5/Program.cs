b95e9cbee057ff04d2cc37ef33a75c5c9ce707d7
Abinet Tamiru <Abinettamiru@outlook.com>
2022-04-30 11:45:24 +0300

    Add project files.

---
+++ Csharp project Q5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_project_Q5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            for(int x=2;x<100;x++)
+            {
+               if(x%2==0)
+                {
+                    Console.Write(x + ", ");
+                }
+               else
+                {
+                    Console.Write(x * -1 + ", ");
+                }
+                
+            }
+        }
+    }
+}
